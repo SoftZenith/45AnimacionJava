37ba6da13932f3efc0d45772d70efdc3db1fa8a4
Bryan Gutierrez <bryan@MBP-de-Bryan.lan>
2018-05-13 17:34:52 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/ittepic/a45animacionjava/MainActivity.java
@@ -0,0 +1,57 @@
+package mx.edu.ittepic.a45animacionjava;
+
+import android.animation.Animator;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.faceIcon);
+    }
+
+    public void onButtonClick(View v) {
+
+        imageView.animate()
+                .scaleX(3)
+                .scaleY(3)
+                .rotationX(360)
+                .rotationY(360)
+                .translationX(-150)
+                .translationY(-250)
+                .setDuration(3000)
+                .setListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+                        Log.i("Animation", "onAnimationStart");
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        Log.i("Animation", "onAnimationEnd");
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+
+                    }
+                });
+    }
+
+}
+
+
